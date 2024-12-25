e63a517a1945050f7a6a7e0990b60b78d65eb113
aiXander <xandersteenbruggex@gmail.com>
2024-12-24 17:45:18 -0800

    make imports more robust

--- download_models.py
+++ download_models.py
@@ -15,7 +15,10 @@ def fixed_get_imports(filename: str | os.PathLike) -> list[str]:
     if not str(filename).endswith("modeling_florence2.py"):
         return get_imports(filename)
     imports = get_imports(filename)
-    imports.remove("flash_attn")
+    try:
+        imports.remove("flash_attn")
+    except:
+        pass
     return imports
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
